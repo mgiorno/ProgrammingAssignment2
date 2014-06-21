81a0fd88c88944342506b93ba60cd365961e3a37
Mario Giorno <westphilly@gmail.com>
2014-06-20 22:32:24 -0400

    git push origin master

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function caches the inverse of the matrix provided if there
+## is no current matrix value that has already been cached.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     m <- NULL
+     
+     set <- function(y) {
+          x <<- y
+          m <<- NULL
+     }
+     
+     get <- function() {
+          x
+     }
+     
+     setmatrix <- function(matrix) {
+          m <<- matrix
+     }
+     
+     getmatrix <- function() {
+          m
+     }
+     
+     list(set = set, get = get,
+          setmatrix = setmatrix,
+          getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
+## The cacheSolve function calculates and caches the inverse matrix of
+## the original matrix supplied as the argument.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     ## Return a matrix that is the inverse of 'x'
+     m <- x$getmatrix()
+     
+     if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+     }
+     
+     data <- x$get()
+     
+     m <- solve(data, ...)
+     
+     x$setmatrix(m)
+     
+     m
 }
